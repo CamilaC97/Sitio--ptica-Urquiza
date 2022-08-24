5bd9e6d7a7961e86872193695cb0c8a264ea4502
Camila Cobo <camy997@gmail.com>
2022-08-24 00:33:23 -0300

    modificaciones crud 2, error sql

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -13,4 +13,32 @@ async function deleteNovedadById(id) {
     return rows;
 }
 
-module.exports = {getNovedades, deleteNovedadById}
+async function insertNovedad(obj) {
+    try {
+        var query ="insert into novedades set ?";
+        var rows = await pool.query(query, [obj])
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}//cierra insert
+
+async function getNovedadById(id) {
+    var query = "select * from novedades where id=?";
+    var rows = await pool.query(query, [id]);
+    return rows [0];
+}
+
+// modificar esos datos
+async function modificarNovedadById (obj, id) {
+    try {
+        var query = "update novedades set ? where id=?";
+        var rows = await pool.query(query, obj [obj, id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+} //cierra modi> update
+
+module.exports = { getNovedades, deleteNovedadById, insertNovedad, getNovedadById, modificarNovedadById }
