0eedc16d023dcd93dacc765079901c5a3a694085
Camila Cobo <camy997@gmail.com>
2022-08-25 10:51:25 -0300

    eliminar y modificar img (no funciona)

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -4,6 +4,7 @@ var novedadesModel = require('../../models/novedadesModel');
 var util =require('util');
 var cloudinary = require('cloudinary').v2;
 const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy =util.promisify(cloudinary.uploader.destroy);
 
 
 /**listar las novedades*/
@@ -41,6 +42,12 @@ router.get('/', async function (req, res, next) {
 /**Eliminar los productos */
 router.get('/eliminar/:id', async (req, res, next) => {
     var id = req.params.id;
+    
+
+    let novedad = await novedadesModel.getNovedadById(id);
+    if (novedad.img_id) {
+        await (destroy(novedad.img_id));
+    }
     await novedadesModel.deleteNovedadById(id);
     res.redirect('/admin/novedades')
 }); //cierra get de eliminar//
@@ -59,6 +66,7 @@ router.post('/agregar', async (req, res, next) => {
             imagen = req.files.imagen;
             img_id = (await uploader(imagen.tempFilePath)).public_id;
         }
+        
         if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
             await novedadesModel.insertNovedad({
                 ...req.body, //spread titulo,subt y cuerpo
@@ -93,10 +101,27 @@ router.get('/modificar/:id', async (req, res, next) => {
 
 router.post('/modificar', async (req, res, next) => {
     try {
+        let img_id = req.body.img_original;
+        let borrar_img_vieja = false;
+        if (req.body.img_delete === "1"){
+            img_id=null
+            borrar_img_vieja= true;
+        } else {
+            if (req.files && Object.keys(req.files).lenght >0){
+                imagen = req.files.imagen;
+                img_id = (await
+                    uploader(imagen.tempFilePath)).public_id;
+                    borrar_img_vieja = true;
+            }
+        }
+        if (borrar_img_vieja && req.body.img_original) {
+            await (destroy(req.body.img_original));
+        }
         var obj = {
             titulo: req.body.titulo,
             subtitulo: req.body.subtitulo,
-            cuerpo: req.body.cuerpo
+            cuerpo: req.body.cuerpo,
+            img_id
         }
 
         console.log(obj) //para ver si trae los datos
